f83aa0b39c77e0e1cbcea4063f0d5b4feda4b419
Rick van Dam <rick.van.dam@avivasolutions.nl>
2019-11-22 12:06:02 +0100

    fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,4 +7,4 @@
 // You can delete this file if you're not using it
 import "./src/styles/global.css"
 import "./src/styles/latolatinfonts.css"
-require("./src/styles/prims-twilight.css")
+import "./src/styles/prims-twilight.css"
