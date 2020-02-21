f785370dfd40091a66426e489a7d6bcd215e4ad3
Rick van Dam <rick-van-dam@outlook.com>
2020-02-21 21:59:53 +0100

    first working search setup

--- src/gatsby-node/config.ts
+++ src/gatsby-node/config.ts
@@ -186,11 +186,11 @@ export const Config = {
       }
     },
     `gatsby-plugin-netlify`,
-    ...(process.env.ALGOLIA_APP_ID ? [{
+    ...(process.env.GATSBY_ALGOLIA_APP_ID ? [{
       resolve: `gatsby-plugin-algolia`,
       options: {
-        appId: process.env.ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.GATSBY_ALGOLIA_ADMIN_KEY,
         queries,
         chunkSize: 10000, // default: 1000
       },
--- src/templates/PostListerTemplate.tsx
+++ src/templates/PostListerTemplate.tsx
@@ -5,12 +5,21 @@ import ListHeader from '../components/ListHeader/ListHeader'
 import BlogLister from '../components/BlogPostLister/BlogPostLister'
 import { PostListerTemplateQuery } from '../types'
 
+// @ts-ignore
+import Search from "../components/Search"
+
 interface PostListerTemplateQueryInterface {
   data: PostListerTemplateQuery
 }
 
+const searchIndices = [
+  { name: `Pages`, title: `Pages`, hitComp: `PageHit` },
+  { name: `Posts`, title: `Blog Posts`, hitComp: `PostHit` },
+]
+
 const PostListerTemplate: React.SFC<PostListerTemplateQueryInterface> = ({ data: { sitePage } }) => (
   <IndexLayout>
+    <Search collapse indices={searchIndices} />
     <ListHeader title=""></ListHeader>
     <BlogLister category={[...sitePage?.context.category ?? []]}></BlogLister>
   </IndexLayout>
